72ce2f56d822b2ef93dea11c411d250574cccebe
Admir Alicic <admir.alicic@gmail.com>
2022-01-09 10:24:45 +0100

    Image optimization and responsive layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Title from "components/Title";
 
 import { getProducts } from "lib/products";
+import ProductCard from "components/ProductCard";
 
 export async function getStaticProps() {
   const products = await getProducts();
@@ -23,12 +24,10 @@ function HomePage({ products }) {
       </Head>
       <main className="px-6 py-4">
         <Title>Next Shop</Title>
-        <ul>
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {products.map((product) => (
             <li key={product.id}>
-              <Link href={`/products/${product.id}`}>
-                <a>{product.title}</a>
-              </Link>
+              <ProductCard product={product} />
             </li>
           ))}
         </ul>
